46a2d067b52f178ab8ccb9814f0c94b17b3659e2
Damon Nunez <dameplays24@gmail.com>
2018-11-28 22:53:44 +0000

    javascript

--- js/script.js
+++ js/script.js
@@ -2,10 +2,111 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+var computerNumber=Math.random();
+ 
+if (computerNumber <.3){
+     $("#computerChoice").html("rock");
+  }
+   if (computerNumber <.6 && computerNumber >=.3){
+     $("#computerChoice").html("paper");
+  }
+   
+// if ( $("Userchoice").val()==="rock"
+     //)
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //GLOBAL VARIABLES
 /* global $ */
 
 
 
 // DOCUMENT READY FUNCTION BELOW
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
