c1d77d0c7fe73f65e8380983c066ee633d7ba95a
Damon Nunez <dameplays24@gmail.com>
2018-11-29 03:19:12 +0000

    lol

--- js/script.js
+++ js/script.js
@@ -5,14 +5,52 @@
 var computerNumber=Math.random();
  
 if (computerNumber <.3){
-     $("#computerChoice").html("rock");
+     $("#computerChoice").html("Rock");
   }
    if (computerNumber <.6 && computerNumber >=.3){
-     $("#computerChoice").html("paper");
+     $("#computerChoice").html("Paper");
+  } 
+  if (computerNumber < 1 && computerNumber>= .6){
+      $("#computerChoice").html("Scissors");
   }
    
-// if ( $("Userchoice").val()==="rock"
-     //)
+ if ( $("Userchoice").val()==="rock"){
+     if ( $("#computerChoice").val()==="Rock"){
+         $("#result").html("Tie");
+         
+     }
+       if ( $("#computerChoice").val()==="Paper"){
+         $("#result").html("computer wins");
+     }
+   if ( $("#computerChoice").val()==="Scissors"){
+       $("#result").html("You win");
+   }
+     }
+
+    if ( $("Userchoice").val()==="Paper"){
+        if ( $("#computerChoice").val()==="Rock"){
+            $("#result").html("You win");
+        }
+         if ( $("#computerChoice").val()==="Paper"){
+             $("#result").html("Tie");
+            
+         }      
+        if ( $("#computerChoice").val()==="Scissors"){
+            $("result").html("Computer wins");
+        }
+       if ( $ ("userChoice").val()==="Scissors"){
+           if ( $("#computerChoice").val()==="Rock"){
+               $("#result").html("Computer wins");
+           }
+           if ( $ ("#computerChoice").val()==="Paper"){
+               $("result").html("You win");
+                }
+                if ( $ ("#computerChoice").val()==="Scissors"){
+                    $("result").html("Tie")
+                }
+       }
+   
+  
        
 
 
